546c45ae0ee58c9899edc78be410ebd3ce0a3e71
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-03 15:37:56 -0500

    Changes how completeOrderPrompt resolves

--- app/controllers/completeOrderC.js
+++ app/controllers/completeOrderC.js
@@ -10,8 +10,8 @@ module.exports.completeOrderPrompt = userId =>
     Promise.all([getSingleOrder(userId), getPaymentOptions(userId)])
       .then((orderTotal, paymentOptions) => 
         prompt.get(
-          createPrompt(orderTotal, paymentOptions), (err, {checkout, paymentOption}) => {
-           checkout === 'N' ? reject() : resolve();
+          createPrompt(orderTotal, paymentOptions), (err, result) => {
+	   resolve(result);
            // patchOrder(paymentOption)
 	   // .then(() => resolve())
         })
