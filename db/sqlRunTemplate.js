4f6cd909000868b04e4c709de859a85770f66cc6
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-04 09:59:42 -0500

    Changes back to outputting to bangazon.sqlite

--- db/sqlRunTemplate.js
+++ db/sqlRunTemplate.js
@@ -1,6 +1,6 @@
 'use strict';
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("db/cli-sprint.sqlite");
+const db = new sqlite3.Database("db/bangazon.sqlite");
 
 /*
 EXAMPLE FUNCTION CALL
