02a89135a3a69da87d661e15fb6dcfb1b091732b
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-04 13:31:19 -0500

    add customer functionality added to model

--- app/models/CustomersM.js
+++ app/models/CustomersM.js
@@ -4,16 +4,43 @@ const path = require('path');
 const db = new Database(path.join(__dirname, '../..', 'db', 'bangazon.sqlite'));
 
 
-// Lists all customers in DB
-// Will be called in #3- Select Active Customer
+// This will be called in #3- Select Active Customer
 module.exports.getAllCustomers = () => {
-  return [{}];
+   return new Promise((resolve, reject) => {
+     db.all(`SELECT * FROM Customers`,
+       (err, customers) => err ? reject(err) : resolve(customers)
+     )
+   });
 }
 
 
-// create new customer
-// will be called in option #1 - Create Customer Account
+// This will be called in option #1 - Create Customer Account
 // Needs to check for duplicates before it posts
-module.exports.addNewCustomer = () => {
-  return 100
+module.exports.addNewCustomer = ({customer_id, first_name, last_name, account_creation_date, street_address, city, state, postal_code, phone_number}) => {
+  return new Promise((resolve, reject) => {
+    db.run(`INSERT INTO Customers (
+      customer_id,
+      first_name,
+      last_name,
+      account_creation_date,
+      street_address,
+      city,
+      state,
+      postal_code,
+      phone_number
+    ) VALUES (
+       ${customer_id == undefined ? null : customer_id},
+       "${first_name}",
+       "${last_name}",
+       "${account_creation_date}",
+       "${street_address}",
+       "${city}",
+       "${state}",
+       "${postal_code}",
+       "${phone_number}"
+   )`,
+   function(err, data){
+    err ? reject(err) : resolve(this.lastID)
+   })
+  })
 }
--- test/customer.test.js
+++ test/customer.test.js
@@ -1,39 +1,59 @@
-const { assert: {equal, isArray, isObject} } = require('chai');
+const { assert: {equal, isArray, isObject, deepEqual} } = require('chai');
 const { getAllCustomers, addNewCustomer } = require('../app/models/CustomersM.js')
+const makeCustomersTable = require('../db/makeCustomersTable')
 
 
 describe('Customers functionality', () => {
   describe('Listing all customers', () => {
     it('Should return an array of objects', () => {
-      isArray(getAllCustomers())
-      isObject(getAllCustomers()[0])
+      getAllCustomers()
+      .then(customers => {
+        console.log('first customer', customers[0]);
+        isArray(customers);
+        isObject(customers[0]);
+      })
     });
+    it('Should bring back the correct customer objects', () => {
+      // First customer from the database, according to our current database schema
+      let firstCustomer = {
+        customer_id: 0,
+        first_name: 'Lennie',
+        last_name: 'Kunze',
+        account_creation_date: '2018-04-03T15:25:47.270Z',
+        street_address: '59842 Earline Gateway',
+        city: 'South Whitneyport',
+        state: 'Indiana',
+        postal_code: '14769-1763',
+        phone_number: '1-510-550-0973'
+      }
+      return getAllCustomers()
+      .then(customers => {
+        deepEqual(firstCustomer, customers[0])
+      })
+    })
+  })
+  afterEach(done => {
+    makeCustomersTable();
+    setTimeout(done, 200);
   })
   describe('Adding a new customer', () => {
     it('Should return the id of the customer you just added', () => {
-      let customer = {
+      let nicolas = {
         customer_id: 100,
         first_name: 'Nicolas',
         last_name: 'Cage',
-        account_creation_date: 'The dawn of time',
+        account_creation_date: '100 BC',
         street_address: 'Rock and Roll Road',
         city: 'Nashville',
         state: 'TN',
         postal_code: '37217',
         phone_number: '888-888-8888'
       }
-      equal(100, addNewCustomer(customer))
+      return addNewCustomer(nicolas)
+      .then(id => {
+        equal(100, id)
+      })
+      
     });
   })
 });
-
-// Pro Tip: Remember, we are testing features, not functions. Require whichever modules you need to test a feature
-// customer_id INTEGER PRIMARY KEY,
-//   first_name TEXT,
-//     last_name TEXT,
-//       account_creation_date TEXT,
-//         street_address TEXT,
-//           city TEXT,
-//             state TEXT,
-//               postal_code TEXT,
-//                 phone_number TEXT
