5bdb9627613fce930a25e0d8c4d3669b6e716a8d
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-04 10:41:16 -0500

    Reformats test/completeOrder.test.js

--- test/completeOrder.test.js
+++ test/completeOrder.test.js
@@ -6,24 +6,24 @@ const { assert: { deepEqual, equal } } = require('chai'),
 describe('completeOrder module', () => {
   describe('completeOrder view', () => {
     describe('createPrompt()', () => {
-	const checkout = {
-                description: 'Your order total is $180. Ready to purchase?\n(Y/N)',
-              },
-              payment = {
-                description: 'Choose a payment option:\n1. AmEx\n2. Visa',
-                pattern: new RegExp('^[1-2]$'),
-              };
-	const [a, b] = createPrompt(180, ['AmEx', 'Visa']);
+      const checkout = {
+            description: 'Your order total is $180. Ready to purchase?\n(Y/N)'
+          },
+          payment = {
+            description: 'Choose a payment option:\n1. AmEx\n2. Visa',
+            pattern: new RegExp('^[1-2]$'),
+          };
+      const [a, b] = createPrompt(180, ['AmEx', 'Visa']);
 
       it('should create a description with the order total included', () => {
-	equal(a.description, checkout.description);
+	       equal(a.description, checkout.description);
       });
       it('should create a description with the customer\'s payment options', () => {
-	equal(b.description, payment.description);
+	       equal(b.description, payment.description);
       });
       it('should create a RegEx for payment that only allows the number of payment options a customer has', () => {
-	deepEqual(b.pattern, payment.pattern);
+	       deepEqual(b.pattern, payment.pattern);
       });
-    }); 
+    });
   });
 });
