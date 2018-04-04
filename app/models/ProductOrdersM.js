7f40330baf09003c99f191640f3de930d20bc2ec
Melissa Bell <melissabell456@gmail.com>
2018-04-04 15:29:50 -0500

    tests and model for adding product to order and collecting sum of order total complete

---
+++ app/models/ProductOrdersM.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('db/bangazon.sqlite');
+
+// adds the data relationship between products and orders
+module.exports.addToProductOrders = (order_id, product) => {
+  return new Promise( (resolve, reject) => {
+    db.run(`INSERT INTO product_orders
+    VALUES(null,  ${product.product_id}, ${order_id}, "${product.current_price}")`, 
+    function(err) {
+      if (err) reject (err);
+      resolve({ productsAdded: this.changes });
+    })
+  })
+}
+
+// gets the sum of a customers order
+module.exports.getOrderTotal = ( { order_id } ) => {
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT SUM(Product_Orders.product_price) AS OrderTotal
+    FROM Product_Orders
+    JOIN Orders ON Orders.order_id = Product_Orders.order_id
+    WHERE Orders.order_id = ${order_id}`, function(err, result){
+      if (err) reject (err);
+      resolve(result);
+    })
+  })
+}
+
+
---
+++ test/ProductOrders.test.js
@@ -0,0 +1,24 @@
+const { assert: {equals, deepEqual, isArray, isObject} } = require('chai');
+const { addToProductOrders, getOrderTotal } = require('../app/models/ProductOrdersM');
+
+describe('ProductOrdersM', () => {
+  let product = {"product_id":1,"current_price":"356.00","title":"Gorgeous Wooden Fish","description":"Tasty","product_type_id":2,"creator_id":12};
+  describe('addProdsToOrder()', () => {
+    it('should return an object', () => {
+      addToProductOrders(1, product)
+      .then( (prodAdded) => {
+        isObject(prodAdded);
+        deepEqual({ productsAdded: 1 }, prodAdded, "this.changes should reflect one product should be added");
+      })
+    });
+  })
+  describe('getOrderTotal()', () => {
+    it('should return an object', () => {
+      getOrderTotal({ order_id: 33})
+      .then( (orderTotal) => {
+        isObject(orderTotal);
+        deepEqual({ OrderTotal: 2633 }, orderTotal);
+      })
+    })
+  })
+})
