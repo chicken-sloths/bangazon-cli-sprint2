be660c8a080a39f9db9d49dd476afd4cc64540dc
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-04 16:33:04 -0500

    built out controller for customers but didn't build test

--- app/controllers/createCustC.js
+++ app/controllers/createCustC.js
@@ -2,15 +2,24 @@
 
 const prompt = require('prompt');
 const promptObj = require('../views/createCustV');
+const {getCustomerByPhoneNumber, addNewCustomer} = require('../models/CustomersM.js')
 
 module.exports.promptNewCustomer = () => {
   return new Promise((resolve, reject) => {
     prompt.get(promptObj, (err, results) => {
       if (err) return reject(err);
-      // TODO: post customer data
-      // TODO: return this promise
-      console.log('customer data to save', results);
-      resolve(results);
+      getCustomerByPhoneNumber(results.phone_number)
+      .then(data => {
+        if(data){
+          return console.log('Sorry, this customer already exists!');
+          // go back to the main menu?
+        } else {
+          return addNewCustomer(results);
+        }
+      })
+      .then(id => {
+        if(id) console.log(`Congrats! You just added a customer with the id of ${id}`)    
+      })
     })
   });
 };
