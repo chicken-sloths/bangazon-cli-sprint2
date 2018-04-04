146c3ed25a01179a7aceea6bc6b41a71413ee65f
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-04 15:57:26 -0500

    i added some comment explanations in the model.

--- app/models/OrdersM.js
+++ app/models/OrdersM.js
@@ -22,6 +22,12 @@ module.exports.checkForActiveOrder = customer_id => {
   });
 };
 
+// If a customer HAS an order already that contains products, but that orders payment type
+//    is null, then it will add the product to that existing order
+// If a customer does not currently have an order (that contains >0 products),
+//    then it will create a new order for them
+// This function should only be called if a customer has an order with products on it
+    // but that order does not have a payment_option_id on it
 module.exports.patchPaymentTypeOntoOrder = (order, payment_option_id) => {
   // This function will add a payment type to an order using patch-like verb like UPDATE 
   return new Promise((resolve, reject)=>{
@@ -45,11 +51,7 @@ module.exports.patchPaymentTypeOntoOrder = (order, payment_option_id) => {
 
 // This will INSERT a new order into the Orders table.
 // It will be used by #5 (on the white board), adding a product to an order. 
-// 2 scenarios:
-// If a customer HAS an order already that contains products, but that orders payment type
-//    is null, then it will add the product to that existing order
-// If a customer does not currently have an order (that contains >0 products),
-//    then it will create a new order for them
+
 module.exports.createNewOrder = order => {
   // This function will create a new order for a customer wihtout an active order
   return new Promise((resolve, reject)=>{
