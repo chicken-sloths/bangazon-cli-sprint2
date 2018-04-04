8357aa2d8dbafa21c9cce55aa0bd98d50ff79d82
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-04 11:30:16 -0500

    added basic passing test for adding a new customer

--- app/models/CustomersM.js
+++ app/models/CustomersM.js
@@ -1,16 +1,19 @@
 'use strict';
-// const { Database } = require('sqlite3').verbose();
-// const path = require('path');
-// const db = new Database(path.join(__dirname, '..', 'db', 'bangazon.sqlite'));
-
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("bangazon.sqlite");
+const { Database } = require('sqlite3').verbose();
+const path = require('path');
+const db = new Database(path.join(__dirname, '../..', 'db', 'bangazon.sqlite'));
 
 
+// Lists all customers in DB
+// Will be called in #3- Select Active Customer
 module.exports.getAllCustomers = () => {
   return [{}];
-  // db.all(`SELECT * FROM Customers`, (customerArray) => {
-  //   console.log(customerArray)
-  // });
 }
 
+
+// create new customer
+// will be called in option #1 - Create Customer Account
+// Needs to check for duplicates before it posts
+module.exports.addNewCustomer = () => {
+  return 100
+}
--- test/customer.test.js
+++ test/customer.test.js
@@ -1,12 +1,39 @@
 const { assert: {equal, isArray, isObject} } = require('chai');
-const { getAllCustomers } = require('../app/models/CustomersM.js')
+const { getAllCustomers, addNewCustomer } = require('../app/models/CustomersM.js')
 
-// Placed here to confirm test file runs properly
-describe('customers functionality', () => {
-  it('should return an array of objects', () => {
-    isArray(getAllCustomers())
-    isObject(getAllCustomers()[0])
-  });
+
+describe('Customers functionality', () => {
+  describe('Listing all customers', () => {
+    it('Should return an array of objects', () => {
+      isArray(getAllCustomers())
+      isObject(getAllCustomers()[0])
+    });
+  })
+  describe('Adding a new customer', () => {
+    it('Should return the id of the customer you just added', () => {
+      let customer = {
+        customer_id: 100,
+        first_name: 'Nicolas',
+        last_name: 'Cage',
+        account_creation_date: 'The dawn of time',
+        street_address: 'Rock and Roll Road',
+        city: 'Nashville',
+        state: 'TN',
+        postal_code: '37217',
+        phone_number: '888-888-8888'
+      }
+      equal(100, addNewCustomer(customer))
+    });
+  })
 });
 
 // Pro Tip: Remember, we are testing features, not functions. Require whichever modules you need to test a feature
+// customer_id INTEGER PRIMARY KEY,
+//   first_name TEXT,
+//     last_name TEXT,
+//       account_creation_date TEXT,
+//         street_address TEXT,
+//           city TEXT,
+//             state TEXT,
+//               postal_code TEXT,
+//                 phone_number TEXT
