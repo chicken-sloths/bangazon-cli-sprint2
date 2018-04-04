ef0c197710cab25f619ec173307ecf0b2649ac6b
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-04 14:54:34 -0500

    New fake data following ERD more closely

--- data/faker/_productsFaker.js
+++ data/faker/_productsFaker.js
@@ -8,9 +8,11 @@ const _generateProducts = ({ productAmount, customerAmount, productTypesAmount }
   for (let i = 0; i < productAmount; i++) {
     const product = {
       product_id: i,
-      price: faker.commerce.price(),
+      current_price: faker.commerce.price(),
       title: faker.commerce.productName(),
       description: faker.commerce.productAdjective(),
+      creation_date: faker.date.recent(),
+      quantity: randomInt(50),
       product_type_id: randomInt(productTypesAmount),
       creator_id: randomInt(customerAmount)
     };
