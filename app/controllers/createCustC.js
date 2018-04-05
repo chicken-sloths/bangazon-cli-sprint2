2d298f7e2c80f77cd8b68cd5ff8f859a784279c6
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-05 09:24:20 -0500

    refactored promise

--- app/controllers/createCustC.js
+++ app/controllers/createCustC.js
@@ -5,21 +5,24 @@ const promptObj = require('../views/createCustV');
 const {getCustomerByPhoneNumber, addNewCustomer} = require('../models/CustomersM.js')
 
 module.exports.promptNewCustomer = () => {
+
   return new Promise((resolve, reject) => {
-    prompt.get(promptObj, (err, results) => {
-      if (err) return reject(err);
+
+    prompt.get(promptObj, (error, results) => {
       getCustomerByPhoneNumber(results.phone_number)
       .then(data => {
         if(data){
-          return console.log('Sorry, this customer already exists!');
-          // go back to the main menu?
+          reject('This customer already exists!');
         } else {
           return addNewCustomer(results);
         }
       })
       .then(id => {
-        if(id) console.log(`Congrats! You just added a customer with the id of ${id}`)    
+        if(id) resolve (id);
       })
-    })
+      .catch(error => {
+        reject(error);
+      });
+    });
   });
 };
