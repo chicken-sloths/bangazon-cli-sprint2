3eb0eed4f0b87475c8907b397b7f67ad279709e9
Melissa Bell <melissabell456@gmail.com>
2018-04-05 15:10:10 -0500

    commit prior to PR review.

--- app/controllers/addCustProdC.js
+++ app/controllers/addCustProdC.js
@@ -2,12 +2,17 @@
 
 const prompt = require('prompt');
 const { addCustProdV } = require('../views/addCustProdV');
+const { activeCustomer } = require('./activeCustC');
 
 module.exports.addCustomerProduct = () => {
   prompt.get(addCustProdV, (err,results) => {
+    // Get customer ID
+    console.log(activeCustomer());
+    // Then the user should be prompted to enter in all appropriate information for a product
     console.log('product name', results.productName)
     console.log('product price', results.productPrice)
     console.log('product description', results.productDescription)
     console.log('product type', results.productType)
+    // And when complete, the product should be added for the active customer
   })
 };
