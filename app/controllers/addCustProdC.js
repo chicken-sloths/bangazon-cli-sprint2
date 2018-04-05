73e796d2b140ef322e3d95b1336663d5aec72320
Melissa Bell <melissabell456@gmail.com>
2018-04-05 15:33:24 -0500

    displaying object of new product to pass and insert into products table

--- app/controllers/addCustProdC.js
+++ app/controllers/addCustProdC.js
@@ -7,12 +7,15 @@ const { activeCustomer } = require('./activeCustC');
 module.exports.addCustomerProduct = () => {
   prompt.get(addCustProdV, (err,results) => {
     // Get customer ID
-    console.log(activeCustomer());
+    // console.log(activeCustomer.id);
     // Then the user should be prompted to enter in all appropriate information for a product
-    console.log('product name', results.productName)
-    console.log('product price', results.productPrice)
-    console.log('product description', results.productDescription)
-    console.log('product type', results.productType)
+    let newProduct = {
+      price: results.productPrice,
+      name: results.productName,
+      description: results.productDescription,
+      productType: results.productType
+    };
+    console.log(newProduct);
     // And when complete, the product should be added for the active customer
   })
 };
