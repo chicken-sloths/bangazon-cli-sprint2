fbf9e095379165dd3d881e6148988af0ba4c9c26
Melissa Bell <melissabell456@gmail.com>
2018-04-05 09:39:47 -0500

    adjusted data specific test for getting order total to run with new db

--- test/ProductOrders.test.js
+++ test/ProductOrders.test.js
@@ -5,7 +5,7 @@ describe('ProductOrdersM', () => {
   let product = {"product_id":1,"current_price":"356.00","title":"Gorgeous Wooden Fish","description":"Tasty","product_type_id":2,"creator_id":12};
   describe('addProdsToOrder()', () => {
     it('should return an object', () => {
-      addToProductOrders(1, product)
+      return addToProductOrders(1, product)
       .then( (prodAdded) => {
         isObject(prodAdded);
         deepEqual({ productsAdded: 1 }, prodAdded, "this.changes should reflect one product should be added");
@@ -14,10 +14,10 @@ describe('ProductOrdersM', () => {
   })
   describe('getOrderTotal()', () => {
     it('should return an object', () => {
-      getOrderTotal({ order_id: 33})
+      return getOrderTotal({ order_id: 2 })
       .then( (orderTotal) => {
         isObject(orderTotal);
-        deepEqual({ OrderTotal: 2633 }, orderTotal);
+        deepEqual({ OrderTotal: 3377 }, orderTotal);
       })
     })
   })
