a7c295f17b2ea58dfb2591f70c06837f91a5db2f
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-05 15:46:30 -0500

    built function to list all products

--- app/controllers/addProdToOrderC.js
+++ app/controllers/addProdToOrderC.js
@@ -2,19 +2,42 @@
 
 const prompt = require('prompt');
 const promptObj = require('../views/addProdToOrderV');
+const { getAllProducts } = require('../models/ProductsM');
+const { getActiveCustomer } = require('../controllers/activeCustC');
+const { checkForActiveOrder } = require('../')
+
+// Promises to print all products and in a numbered list to the terminal
+const listAllProds = () => {
+  return new Promise((resolve, reject) => {
+    getAllProducts()
+      .then(products => {
+        console.log('Here are all the products:');
+        products.forEach((product, i) => {
+          console.log(`${i}. ${product.title}`)
+        });
+      });
+  })
+}
+
+
 
 module.exports.addProductToOrder = () => {
   return new Promise((resolve, reject) => {
-    console.log('Here are all the products:');
-    // TODO: get all Products
-    prompt.get(promptObj, (err, results) => {
-      if (err) return reject(err);
-      // TODO: check if active customer has active order
-      // TODO: if not, create new
-      // TODO: add product to order
-      // TODO: return that promise (resolve/reject based on that promise's results)
-      console.log('Id of object to add to order', results);
-      resolve(results);
-    });
+    listAllProds()
+      .then(() => {
+        prompt.get(promptObj, (err, results) => {
+          if (err) return reject(err);
+          let customerId = getActiveCustomer();
+          console.log('should be active customer id', customerId);
+          return checkForActiveOrder(customerId);
+          // TODO: check if active customer has active order
+          // TODO: if not, create new
+          // TODO: add product to order
+          // TODO: return that promise (resolve/reject based on that promise's results)
+          console.log('Id of object to add to order', results);
+          resolve(results);
+        });
+
+    })
   });
 };
