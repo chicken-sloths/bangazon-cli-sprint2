0c17ac09adb7b95e88fed4a106be90337e105c8b
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-06 15:26:13 -0500

    Provides documentation for ProductOrdersM

--- app/models/ProductOrdersM.js
+++ app/models/ProductOrdersM.js
@@ -1,13 +1,24 @@
 'use strict';
-
+/**
+ * A module that accesses the productOrders table in the bangazon.sqlite DB
+ * @module productOrdersModel
+ */
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('db/bangazon.sqlite');
 
-// adds the data relationship between products and orders
+/**
+ * @function addToProductOrders
+ * @param {number} order_id - ID of the order that the product is being added to the order
+ * @param {object} product
+ * @param {number} product.product_id - ID of the product that is being added to the order
+ * @param {string} product.current_price - Current price of the product
+ * @returns {Promise} A promise representing the number of changes to Product_Orders (if successful, should be 1)
+ * @description Adds a order to a product by adding a new entry to the Product_Orders table with all the requisite data detailing that relationship. product.current_price saves the price at which the product was actually added to their cart
+ */
 module.exports.addToProductOrders = (order_id, product) => {
   return new Promise( (resolve, reject) => {
     db.run(`INSERT INTO product_orders
-    VALUES(null,  ${product.product_id}, ${order_id}, "${product.current_price}")`, 
+    VALUES(null,  ${product.product_id}, ${order_id}, "${product.current_price}")`,
     function(err) {
       if (err) return reject (err);
       resolve(this.changes);
@@ -15,6 +26,13 @@ module.exports.addToProductOrders = (order_id, product) => {
   })
 }
 
+/**
+ * @function getOrderTotal
+ * @param {object} order
+ * @param {number} order_id - ID of the order to be totalled
+ * @returns {Promise} A promise representing the total cost of the order as a string
+ * @description Sums the prices for each product associated with the given order id
+ */
 // returns sum of a customers order
 module.exports.getOrderTotal = ( { order_id } ) => {
   return new Promise((resolve, reject) => {
@@ -27,5 +45,3 @@ module.exports.getOrderTotal = ( { order_id } ) => {
     })
   })
 }
-
-
