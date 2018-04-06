6844437dc59f02d7ccc504fe81a12f22e18a52b9
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-06 14:41:40 -0500

    fixed the weird white errors but the test passed thing. now have 3 tests failing

--- test/productsM.test.js
+++ test/productsM.test.js
@@ -20,31 +20,24 @@ let sampleProduct = {
 
 describe('createProduct()', () => {
   it('should resolve into an integer', () => {
-    createProduct(sampleProduct)
+    return createProduct(sampleProduct)
       .then(response => {
         assert.isNumber(response);
-      })
-      .catch(err => console.log('createProduct error', err));
+      });
   });
   it('should reject if it doesn\'t receive a name, price, description, and productType', () => {
-    createProduct(sampleProduct)
+    return createProduct(sampleProduct)
       .then(response => {
         assert.isTrue(true);
-      })
-      .catch(err => {
-        assert.isTrue(false);
       });
   });
 });
 
 describe('getProduct(id)', () => {
   it('should resolve into an object', () => {
-    getProduct(5)
+    return getProduct(5)
       .then(response => {
         assert.isObject(response);
-      })
-      .catch(err => {
-        console.log('getProduct error', err);
       });
   });
   it('should return false if it doesn\'t exist', () => {
@@ -60,44 +53,40 @@ describe('getProduct(id)', () => {
 
 describe('getAllProducts()', () => {
   it('should resolve into an array of objects', () => {
-    getAllProducts()
+    return getAllProducts()
       .then(response => {
         assert.isArray(response);
         assert.isObject(response[0]);
-      })
-      .catch(err => console.log('getAllProducts error', err));
+      });
   });
 });
 
 describe('getProductsByCreator', () => {
   it('should resolve into an array of objects', () => {
-    getProductsByCreator(2)
+    return getProductsByCreator(2)
       .then(products => {
         assert.isArray(products);
         assert.isObject(products[0]);
-      })
-      .catch(err => console.log('getProductsByCreator error', err));
+      });
   });
   it('creator 2 should have 6 products', () => {
-    getProductsByCreator(2)
+    return getProductsByCreator(2)
       .then(response => {
         assert.equal(response.length, 6);
-      })
-      .catch(err => console.log('getProductsByCreator error', err));
+      });
   });
 });
 
 describe('deleteProduct()', () => {
   it('resolves into a nonzero number', () => {
-    createProduct(sampleProduct)
+    return createProduct(sampleProduct)
       .then(id => {
         return deleteProduct(id);
       })
       .then(response => {
         assert.isNumber(response);
         assert.isAbove(response, 0);
-      })
-      .catch(err => console.log('deleteProduct error', err));
+      });
   });
   it('throws a fit if you try to delete something that isn\'t there', () => {
     deleteProduct(1000)
@@ -112,35 +101,31 @@ describe('deleteProduct()', () => {
 
 describe('getAllStockedProducts()', () => {
   it('should resolve into an array of objects', () => {
-    getAllStockedProducts()
+    return getAllStockedProducts()
       .then(response => {
         assert.isArray(response);
         assert.isObject(response[0]);
-      })
-      .catch(err => console.log('getAllStockedProducts error', err));
+      });
   });
   it('should have a length of 43', () => {
-    getAllStockedProducts()
+    return getAllStockedProducts()
       .then(response => {
         assert.equal(response.length, 43);
-      })
-      .catch(err => console.log('getAllStockedProducts error', err));
+      });
   });
 });
 
 describe('getQuantityRemaining()', () => {
   it('should resolve into a number', () => {
-    getQuantityRemaining(3)
+    return getQuantityRemaining(3)
       .then(qty => {
         assert.isNumber(qty);
-      })
-      .catch(err => console.log('getQuantityRemaining error', err));
+      });
   });
   it('getQuantityRemaining(40) should return 7', () => {
-    getQuantityRemaining(40)
+    return getQuantityRemaining(40)
       .then(qty => {
         assert.equal(qty, 7);
-      })
-      .catch(err => console.log('getQuantityRemaining() error', err));
+      });
   });
 });
