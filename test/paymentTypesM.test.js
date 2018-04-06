623dc00211b7b48bac50942003639c7247849a6d
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-06 11:42:15 -0500

    have 2 tests passing for the payment types model

--- test/paymentTypesM.test.js
+++ test/paymentTypesM.test.js
@@ -1,5 +1,5 @@
 
-const { assert: { isFunction, isArray } } = require('chai');
+const { assert: { isFunction, isArray, isObject } } = require('chai');
 const { getAllPaymentTypes } = require('../app/models/PaymentTypesM');
 const makePaymentTypesTable = require('../db/makePaymentTypesTable');
 const { generateSqlTable } = require('../db/sqlRunTemplate');
@@ -10,15 +10,16 @@ beforeEach(done => {
     .then(() => done());
 });
 
-describe("getAllPaymentTypes function ", ()=>{
+describe("getAllPaymentTypes() ", ()=>{
   it("should be a function", ()=>{
     isFunction(getAllPaymentTypes);
   });
   
-  it("should return an array of the payment types", ()=>{
+  it("should return an array of the payment types object", ()=>{
     getAllPaymentTypes()
     .then(paymentTypes=>{
       isArray(paymentTypes);
+      isObject(paymentTypes[0]);
     });
   });
 })
