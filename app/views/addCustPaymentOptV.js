c700ab58f362fa84a1be979e0192388cbe6aa883
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-06 11:49:48 -0500

    removed the semi-colons in the addCustPaymentOptV file so they arent doubling up

--- app/views/addCustPaymentOptV.js
+++ app/views/addCustPaymentOptV.js
@@ -1,13 +1,13 @@
 module.exports.paymentOptionPrompts = [
   {
     name: 'paymentType',
-    description: 'Please enter a payment option :',
+    description: 'Please enter a payment option',
     required: true,
     type: 'string'
   },
   {
     name: 'accountNumber',
-    description: 'Please enter your account number :',
+    description: 'Please enter your account number',
     required: true,
     pattern: '^[0-9 ]*$'
   }
