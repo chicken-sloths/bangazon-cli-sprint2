ea2d067aa4b9befa255f313f79d2e33cc709bbeb
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-06 14:20:24 -0500

    fixed the test for adding a new payment option

--- test/PaymentOptionsM.test.js
+++ test/PaymentOptionsM.test.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const { assert: { deepEqual, equal, isEmpty } } = require('chai'),
+const { assert: { deepEqual, equal, isEmpty, isNumber } } = require('chai'),
       {
 	       getPaymentOptionsForCustomer,
 	       addPaymentOption
       } = require('../app/models/PaymentOptionsM'),
       { generateSqlTable } = require('../db/sqlRunTemplate'),
       PaymentOptionsTable = require('../db/makePaymentOptionsTable');
-      // Lord have mercy on this syntax, my dudes ^^^
+      // ^^^ Lord have mercy on this syntax, my dudes ^^^
 
 describe('PaymentOptionsModel module', () => {
   describe('getPaymentOptionsForCustomer()', () => {
@@ -37,14 +37,14 @@ describe('PaymentOptionsModel module', () => {
 
   describe('addPaymentOption()', () => {
     it('should add a payment option for the given customer id', () => {
-      const obj = {
-      	type: "American Express",
+      const fakePaymentOption = {
+      	payment_type: 3,
       	account_number: "17251823",
       	customer_id: 23
       };
 
-      return addPaymentOption(obj)
-	     .then(payOptId => equal(payOptId, 52));
+      return addPaymentOption(fakePaymentOption)
+	     .then(payOptId => isNumber(payOptId));
     });
   });
 });
