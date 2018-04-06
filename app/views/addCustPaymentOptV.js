051faafa55d2d35679f1fbb9f686ff0cb53a1eae
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-06 11:11:48 -0500

    i think i finally found the issue. db looks good

--- app/views/addCustPaymentOptV.js
+++ app/views/addCustPaymentOptV.js
@@ -3,7 +3,7 @@ module.exports.paymentOptionPrompts = [
     name: 'paymentType',
     description: 'Please enter a payment option : ',
     required: true,
-    type: 'string'
+    payment_type: 'string'
   },
   {
     name: 'accountNumber',
