c3e97616c2a18d6c8eadc37c0189a9861b37c4ae
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-09 14:03:13 -0500

    switched from regex to conform, hallelujah

--- app/views/activeCustV.js
+++ app/views/activeCustV.js
@@ -1,14 +1,12 @@
 'use strict';
 
 module.exports.createPrompt = numberOfCustomers => {
-    const digits = (numberOfCustomers-1).toString().split('');
-    const customersRegEx = new RegExp(`^[0-${+digits[0]}][0-${+digits[1]}]*$`);
-   
+
     const activeCustomerPrompt = [{
         name: 'custId',
         description: 'Please choose a customer from the list.',
         required: true,
-        pattern: customersRegEx
+        conform: userChoice => userChoice >= 0 && userChoice < numberOfCustomers ? true : false
     }]
 
     return activeCustomerPrompt;
