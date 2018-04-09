36c77f2d0146e10c6846129d054ec01699b22df4
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-04-09 13:47:54 -0500

    valiate adddress on alphanumeric and spaces

--- app/views/createCustV.js
+++ app/views/createCustV.js
@@ -3,42 +3,36 @@
 module.exports = [{
   name: 'first_name',
   description: 'Enter customer first name',
-  type: 'string',
   pattern: /^[\w]+$/,
   required: true
 }, {
   name: 'last_name',
   description: 'Enter customer last name',
-  type: 'string',
   pattern: /^[\w]+$/,
   required: true
 }, {
   name: 'street_address',
   description: 'Enter street address',
-  type: 'string',
+  pattern: /^[\w\s-]+$/,
   required: true
 }, {
   name: 'city',
   description: 'Enter city',
-  type: 'string',
   pattern: /^[\w\s-]+$/,
   required: true
 }, {
   name: 'state',
   description: 'Enter state (KY)',
-  type: 'string',
   pattern: /^[A-Z]{2}$/,
   required: true
 }, {
   name: 'postal_code',
   description: 'Enter postal code',
-  type: 'string',
   pattern: /^\d{5}$/,
   required: true
 }, {
   name: 'phone_number',
   description: 'Enter phone number (xxx-yyy-zzzz)',
-  type: 'string',
   pattern: /^\d{0,1}[-\s]*\(*\d{3}\)*[-\s]*\d{3}[-\s]*\d{4}$/,
   required: true
 }];
