ccab40b65c27d06b9303265b53fd963ac9142926
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-10 10:30:13 -0500

    added docs for active customer controller

--- app/controllers/activeCustC.js
+++ app/controllers/activeCustC.js
@@ -1,19 +1,44 @@
 'use strict';	
+/**
+ * A module that sets and gets the active customer for the Bangazon CLI
+ * @module activeCustomerController
+ */
+
+
 const { createPrompt } = require('../views/activeCustV');
 const { getAllCustomers } = require('../models/CustomersM');
 const prompt = require('prompt');
 
-
+/** 
+ * @constant activeCustomer
+ * @type {object}
+ * @default
+ * @description Stores the active customer id, which can be accessed in other modules through the the exported function [getActiveCustomer Function]{@link module:activeCustomerController.function:getActiveCustomer}
+*/
 const activeCustomer = {
   id: null
 }	
 
+/**
+ * @function setActiveId
+ * @param {string} customerId accepts the id of the customer to be activated (Prompt returns it as a string, but it's a numerical id)
+ * @description Called internally in [setActiveCustomer]{@link module:activeCustomerController.function:setActiveCustomer} and sets the id property of [the activeCustomer object]{@link module:activeCustomerController.constant:activeCustomer}
+ */
 const setActiveId = id => {
   activeCustomer.id = id;
 }
 
+/**
+ * @function getActiveCustomer
+ * @returns {string} returns the customer id, which is stored as a string
+ */
 module.exports.getActiveCustomer = () => activeCustomer.id;
 
+/**
+ * @function setActiveCustomer
+ * @returns {Promise} 
+ * @description Called in ui.js in option two, promises to a) list all the customers, b) prompt the user for a customer id to activate, and c) set the given customer as active by calling [setActiveId]{@link module:activeCustomerController.function:setActiveId}
+ */
 module.exports.setActiveCustomer = () => {
   return new Promise ((resolve, reject) => {    
     getAllCustomers()
